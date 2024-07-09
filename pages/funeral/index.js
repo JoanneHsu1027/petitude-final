ecd796046a15766522184d5fc8fc0898d7ed7c8c
JeremyLai99 <jeremylai0728@gmail.com>
2024-07-09 10:15:31 +0800

    [merge fix issues]

--- pages/funeral/index.js
+++ pages/funeral/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Layout from '@/components/layout/layout'
 import Head from 'next/head'
 
+export default function Home() {
   return (
     <Layout title="首頁" pageName="home">
       <Head>
