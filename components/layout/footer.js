7818bd049e593495872c6fd5494965bbbd6a1d70
trent3281 <trent3281@gmail.com>
2024-07-12 11:40:49 +0800

    footer styles

--- components/layout/footer.js
+++ components/layout/footer.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import PlatformStyles from '@/styles/platform/platform-style.module.css'
+import styles from '@/styles/footer/footer-style.module.css'
 
 export default function Footer() {
   return (
@@ -11,35 +11,35 @@ export default function Footer() {
         <div className="row ms-3">
           <div className="col-sm-3 d-flex flex-column border-start border-dark mb-3">
             <h6 className="m-0 fw-bolder">會員專區</h6>
-            <a className={`${PlatformStyles.AReset}`} href="#">
+            <a className={`${styles.AReset}`} href="#">
               會員權益
             </a>
-            <a className={`${PlatformStyles.AReset}`} href="#">
+            <a className={`${styles.AReset}`} href="#">
               隱私權政策
             </a>
           </div>
           <div className="col-sm-3 d-flex flex-column border-start border-dark mb-3">
             <h6 className="m-0 fw-bolder">購物須知</h6>
-            <a className={`${PlatformStyles.AReset}`} href="#">
+            <a className={`${styles.AReset}`} href="#">
               付款與配送方式
             </a>
-            <a className={`${PlatformStyles.AReset}`} href="#">
+            <a className={`${styles.AReset}`} href="#">
               退換貨說明
             </a>
           </div>
           <div className="col-sm-3 d-flex flex-column border-start border-dark mb-3">
             <h6 className="m-0 fw-bolder">關於我們</h6>
-            <a className={`${PlatformStyles.AReset}`} href="#">
+            <a className={`${styles.AReset}`} href="#">
               品牌故事
             </a>
             <p className="m-0">統一編號: 12345678</p>
           </div>
           <div className="col-sm-3 d-flex flex-column border-start border-dark mb-3">
             <h6 className="m-0 fw-bolder">聯絡我們</h6>
-            <a className={`${PlatformStyles.AReset}`} href="#">
+            <a className={`${styles.AReset}`} href="#">
               Facebook粉絲團
             </a>
-            <a className={`${PlatformStyles.AReset}`} href="#">
+            <a className={`${styles.AReset}`} href="#">
               Line官方粉絲團
             </a>
             <p className="m-0">客服專線: 02-12345678</p>
