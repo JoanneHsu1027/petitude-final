509797e5252ee5f0142b14a8f9cada6f8c832ac1
Angela <angelakao519@gmail.com>
2024-07-14 21:11:09 +0800

    三個頁面能夠正確帶入localstorage資料分別進入下一頁(card.js->booking-list.js->confirm.js)

--- components/funeral/project/card.js
+++ components/funeral/project/card.js
@@ -1,45 +1,47 @@
 import React, { useState, useEffect } from 'react'
 import Styles from '@/components/funeral/project/card.module.css'
 import { useRouter } from 'next/router'
-import { PJ_LIST } from '@/configs/api-path'
+import { PJ_LIST } from '@/configs/funeral/api-path'
 import axios from 'axios'
 
 export default function Card() {
   const router = useRouter()
-  
+
   const [data, setData] = useState({
     success: false,
     rows: [],
   })
 
-  // const cardData = [
-  //   {
-  //     title: '溫馨寵物 -個別羽化',
-  //     description:
-  //       '生命雖短，愛卻無疆。我們為愛寵設計的告別儀式，不僅是對生命的尊重，更是對每份愛的證明。無論何時決定，免費冰存都將為您騰出思考的空間。在這份旅程終點，讓我們攜手將愛寵的故事。',
-  //     details: '免費結緣往生被/十字被\n\n免費靈體冰存14天\n\n免費懷念骨灰罐',
-  //     imageSrc: '/funeral/Vector 20.png',
-  //     link: '/pages/cart',
-  //   },
-  //   {
-  //     title: '尊榮寵物 -個別羽化',
-  //     description:
-  //       '生命雖短，愛卻無疆。我們為愛寵設計的告別儀式，不僅是對生命的尊重，更是對每份愛的證明。無論何時決定，免費冰存都將為您騰出思考的空間。在這份旅程終點，讓我們攜手將愛寵的故事。',
-  //     details: '免費結緣往生被/十字被\n\n免費靈體冰存14天\n\n免費懷念骨灰罐',
-  //     imageSrc: '/funeral/index_n5.png',
-  //     link: '/pages/funeral',
-  //   },
-  //   {
-  //     title: '朋友寵物 - 集體羽化',
-  //     description:
-  //       '生命雖短，愛卻無疆。我們為愛寵設計的告別儀式，不僅是對生命的尊重，更是對每份愛的證明。無論何時決定，免費冰存都將為您騰出思考的空間。在這份旅程終點，讓我們攜手將愛寵的故事。',
-  //     details: '免費結緣往生被/十字被\n\n 免費靈體冰存14天\n\n 免費懷念骨灰罐',
-  //     imageSrc: '/funeral/Vector 21.png',
-  //     link: '/pages/funeral',
-  //   },
-  // ]
+  const cardData = [
+    {
+      title: '溫馨寵物 -個別羽化',
+      description:
+        '生命雖短，愛卻無疆。我們為愛寵設計的告別儀式，不僅是對生命的尊重，更是對每份愛的證明。無論何時決定，免費冰存都將為您騰出思考的空間。在這份旅程終點，讓我們攜手將愛寵的故事。',
+      details: '免費結緣往生被/十字被\n\n免費靈體冰存14天\n\n免費懷念骨灰罐',
+      price: 'NTD 7000',
+      imageSrc: '/funeral/Vector 20.png',
+      link: '/funeral/funeral/booking-list',
+    },
+    {
+      title: '尊榮寵物 -個別羽化',
+      description:
+        '生命雖短，愛卻無疆。我們為愛寵設計的告別儀式，不僅是對生命的尊重，更是對每份愛的證明。無論何時決定，免費冰存都將為您騰出思考的空間。在這份旅程終點，讓我們攜手將愛寵的故事。',
+      details: '免費結緣往生被/十字被\n\n免費靈體冰存14天\n\n免費懷念骨灰罐',
+      price: 'NTD 9000',
+      imageSrc: '/funeral/index_n5.png',
+      link: '/funeral/funeral/booking-list',
+    },
+    {
+      title: '朋友寵物 - 集體羽化',
+      description:
+        '生命雖短，愛卻無疆。我們為愛寵設計的告別儀式，不僅是對生命的尊重，更是對每份愛的證明。無論何時決定，免費冰存都將為您騰出思考的空間。在這份旅程終點，讓我們攜手將愛寵的故事。',
+      details: '免費結緣往生被/十字被\n\n 免費靈體冰存14天\n\n 免費懷念骨灰罐',
+      price: 'NTD 4000',
+      imageSrc: '/funeral/Vector 21.png',
+      link: '/funeral/funeral/booking-list',
+    },
+  ]
 
-  // useEffect放在return上面(串後端api)
   useEffect(() => {
     const fetchData = async () => {
       const page = router.query.page || 1
@@ -54,8 +56,7 @@ export default function Card() {
       }
 
       const searchKeyword = new URLSearchParams(keyword)
-
-      const res = await axios.get(`${PJ_LIST}?${searchKeyword.toString()}`)
+      await axios.get(`${PJ_LIST}?${searchKeyword.toString()}`)
     }
     fetchData()
   }, [router.query])
@@ -63,49 +64,55 @@ export default function Card() {
   return (
     <div title="生前契約" pageName="project-list">
       <div className="row">
-        {data.rows.length > 0 ? (
-          data.rows.map((r, i) => (
-            <div className="col-md-4 text-center p-2" key={i}>
-              <div className={Styles.card}>
-                <img className={Styles.cardImage} src={r.imageSrc} alt="" />
-                <div className={Styles.cardContent}>
-                  <h5>{r.project_name}</h5>
-                  <h6>{r.project_content}</h6>
+        {cardData.map((card, i) => (
+          <div className="col-md-4 text-center p-2" key={i}>
+            <div className={Styles.card}>
+              <img className={Styles.cardImage} src={card.imageSrc} alt="" />
+              <div className={Styles.cardContent}>
+                <h5>{card.title}</h5>
+                <h6>{card.description}</h6>
+                <div className="d-flex justify-content-between">
                   <div>
-                    {typeof r.project_content === 'string' &&
-                      r.project_content
-                        .split('\n\n')
-                        .map((project_content, idx) => (
-                          <p key={idx} className={Styles.cardDetails}>
-                            {project_content}
-                          </p>
-                        ))}
+                    {/* details的文字部分, 做分行呈現 */}
+                    {typeof card.details === 'string' &&
+                      card.details.split('\n\n').map((detail, idx) => (
+                        <p key={idx} className={Styles.cardDetails}>
+                          {detail}
+                        </p>
+                      ))}
                   </div>
-                  <div className="d-flex justify-content-end align-items-right mb-3">
-                    <button
-                      type="button"
-                      className={`btnPlan btn btn-warning ${Styles.btnPlan}`}
-                      onClick={() => {
-                        router.push('/funeral/funeral/booking-list')
-                      }}
-                    >
-                      選擇方案
-                    </button>
+                  <div style={{ marginTop: '30px', marginRight: '5px' }}>
+                    {card.price}
+                  </div>
+                </div>
 
-                    <button
-                      type="button"
-                      className={`btnPlan1 btn btn-warning ${Styles.btnPlan1}`}
-                    >
-                      加入購物車
-                    </button>
-                  </div>
+                <div className="d-flex justify-content-end align-items-right mb-3">
+                  {/* 按鈕點擊某方案, 則將該方案資料帶入localstorage存儲, 進入booking-list時在帶出該方案資料 */}
+                  <button
+                    type="button"
+                    className={`btnPlan btn btn-warning ${Styles.btnPlan}`}
+                    onClick={() => {
+                      localStorage.setItem(
+                        'selectedOption',
+                        JSON.stringify(card),
+                      )
+                      router.push(card.link)
+                    }}
+                  >
+                    選擇方案
+                  </button>
+                  <button
+                    type="button"
+                    className={`btnPlan1 btn btn-warning ${Styles.btnPlan1}`}
+                    onClick={() => router.push('/funeral/funeral/cart')}
+                  >
+                    加入購物車
+                  </button>
                 </div>
               </div>
             </div>
-          ))
-        ) : (
-          <p>載入中...</p> // 在資料加載完成前顯示加載訊息
-        )}
+          </div>
+        ))}
       </div>
     </div>
   )
