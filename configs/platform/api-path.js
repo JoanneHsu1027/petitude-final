ab061caf775050f2e9b28820ece882c6d2c81a97
trent3281 <trent3281@gmail.com>
2024-07-16 22:59:00 +0800

    u

--- configs/platform/api-path.js
+++ configs/platform/api-path.js
@@ -1,6 +1,8 @@
 export const API_SERVER = 'http://localhost:3001'
 
-//platform   class
+//class
 export const CLASS = `${API_SERVER}/class/api`
-//platform   article
+//article
 export const ARTICLE = `${API_SERVER}/article/api`
+//add article
+export const ARTICLE_ADD_POST = `${API_SERVER}/article/add`
