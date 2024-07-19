664c53971ec69fda956aa716c6c873c1a05e81a4
Angela <angelakao519@gmail.com>
2024-07-19 16:22:51 +0800

    修改輪播圖

---
+++ components/funeral/funeral/carousel.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import ReactBootstrapCarousel from 'react-bootstrap-carousel'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'react-bootstrap-carousel/dist/react-bootstrap-carousel.css'
+
+export default function Carousel() {
+  return (
+    <>
+      {/* <!-- carousel --> */}
+      <div className="containerp-0">
+        <div className="row m-0">
+          <div className="col-12 p-0">
+            <div
+              id="carouselExampleSlidesOnly"
+              className="carousel slide"
+              data-bs-ride="carousel"
+            >
+              <div className="carousel-inner">
+                <div className="carousel-item active">
+                  <img
+                    src="/funeral/appointment1.png"
+                    className="d-block w-100"
+                    alt="..."
+                  />
+                </div>
+                <div className="carousel-item">
+                  <img
+                    src="/funeral/appointment2.png"
+                    className="d-block w-100"
+                    alt="..."
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* <!-- carousel --> */}
+    </>
+  )
+}
