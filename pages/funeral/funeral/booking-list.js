e319b2a98bf78d45468c7ef79a914ee149372921
Angela <angelakao519@gmail.com>
2024-07-19 15:02:35 +0800

    修改購物車訂單頁面

--- pages/funeral/funeral/booking-list.js
+++ pages/funeral/funeral/booking-list.js
@@ -4,22 +4,18 @@ import { useRouter } from 'next/router'
 import styles from '@/styles/funeral/booking-list.module.css'
 import axios from 'axios'
 import swal from 'sweetalert2'
-import { RequestList } from '@/configs/estore/api-path'
+import { RL } from '@/configs/funeral/api-path'
+import { useCart1 } from '@/contexts/funeral/CartContext1'
+import { z } from 'zod'
 
 export default function BookingList() {
-  const [selectedPaymentMethod, setSelectedPaymentMethod] = useState(false)
   const router = useRouter()
-  const [selectedBillMethod, setSelectedBillMethod] = useState(false)
-  const [cartItems, setCartItems] = useState([])
-  const [counties, setCounties] = useState([])
-  const [cities, setCities] = useState([])
+  const [selectedBillMethod, setSelectedBillMethod] = useState(false) // 發票方式
+  const [cartProjects, setCartProjects] = useState([]) //儲存購物車項目
+  const [counties, setCounties] = useState([]) //儲存縣市
+  const [cities, setCities] = useState([]) // 儲存鄉鎮
   const [selectedCounty, setSelectedCounty] = useState('')
   const [selectedCity, setSelectedCity] = useState('')
-
-  const paymentMethodMapping = {
-    creditCard: '信用卡',
-    otherMethods: 'Line Pay',
-  }
 
   const billMethodMapping = {
     phoneBill: '手機載具',
@@ -28,25 +24,47 @@ export default function BookingList() {
     companyBill: '公司發票',
   }
 
+  // 用於儲存結帳表單的值
+  const [formData, setFormData] = useState({
+    paymentMethod: '',
+    creditCardNumber: '',
+    expiryDate: '',
+    cvv: '',
+    buyerName: '',
+    mobile: '',
+    telephone: '',
+    county: '',
+    city: '',
+    address: '',
+    billMethod: '',
+    billNumber: '',
+  })
+  const [formDataErrors, setFormDataErrors] = useState({
+    buyerName: '',
+    mobile: '',
+  })
+  const { clearCart1 } = useCart1()
+
   useEffect(() => {
-    // 從 localStorage 讀取購物車資料
-    const storedCart = localStorage.getItem('funeralshoppingcart')
-    if (storedCart) {
-      setCartItems(JSON.parse(storedCart))
+    // 使用hook 從 localStorage 讀取購物車資料, 並存到cartItems裡
+    const storedCart1 = localStorage.getItem('funeralShoppingCart')
+    if (storedCart1) {
+      setCartProjects(JSON.parse(storedCart1))
     }
   }, [])
 
-  const totalPrice = cartItems.reduce(
-    (total, item) => total + item.product_price * item.qty,
+  // 計算項目總價, 儲存在totalPrice裡
+  const totalPrice = cartProjects.reduce(
+    (total, project) => total + project.project_price * project.qty,
     0,
   )
 
+  // 使用hook獲取所有縣市, 並存至counties裡
   useEffect(() => {
-    // 獲取所有縣市
     const fetchCounties = async () => {
       try {
         const response = await axios.get(
-          'http://localhost:3001/product/counties',
+          'http://localhost:3001/project/counties',
         )
         if (response.data.success) {
           setCounties(response.data.data)
@@ -59,13 +77,13 @@ export default function BookingList() {
     fetchCounties()
   }, [])
 
+  // 當選擇的縣市selectedCounty 改變時，獲取對應的鄉鎮市區, 存到cities裡
   useEffect(() => {
-    // 當選擇的縣市改變時，獲取對應的鄉鎮市區
     const fetchCities = async () => {
       if (selectedCounty) {
         try {
           const response = await axios.get(
-            `http://localhost:3001/product/cities/${selectedCounty}`,
+            `http://localhost:3001/project/cities/${selectedCounty}`,
           )
           if (response.data.success) {
             setCities(response.data.data)
@@ -81,6 +99,7 @@ export default function BookingList() {
     fetchCities()
   }, [selectedCounty])
 
+  // 下面兩個分别處理縣市和鄉鎮市區的選擇變更，並更新對應的狀態及表單數據
   const handleCountyChange = (e) => {
     const countyId = e.target.value
     setSelectedCounty(countyId)
@@ -100,78 +119,115 @@ export default function BookingList() {
       cityId: cityId,
     })
   }
-
-  // 讀取結帳資料
-  const [formData, setFormData] = useState({
-    paymentMethod: '',
-    creditCardNumber: '',
-    expiryDate: '',
-    cvv: '',
-    buyerName: '',
-    mobile: '',
-    telephone: '',
-    county: '',
-    city: '',
-    address: '',
-    billMethod: '',
-    billNumber: '',
+  // 表單欄位驗證
+  const schemaForm = z.object({
+    buyerName: z.string().min(2, { message: '姓名至少兩個字' }),
+    mobile: z.string().regex(/^09\d{8}$/, { message: '請填寫正確的手機格式' }),
   })
 
+  // 表單若有輸入或其他變化, 則更新formData狀態
   const handleInputChange = (e) => {
-    setFormData({
-      ...formData,
-      [e.target.id]: e.target.value,
-    })
+    const { id, value } = e.target
+    console.log(id, value)
+
+    const newForm = { ...formData, [id]: value }
+    setFormData(newForm)
+
+    // 重置 formDataErrors
+    const newFormErrors = {
+      buyerName: '',
+      mobile: '',
+    }
+
+    const result = schemaForm.safeParse(newForm)
+    console.log(JSON.stringify(result, null, 4))
+
+    if (!result.success && result?.error?.issues?.length) {
+      for (let issue of result.error.issues) {
+        newFormErrors[issue.path[0]] = issue.message
+      }
+    }
+    setFormDataErrors(newFormErrors)
   }
 
-  const handleSubmit = async () => {
+  // 用於提交表單數據
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+
+    // 進行全面驗證
+    const result = schemaForm.safeParse(formData)
+
+    if (!result.success) {
+      const newFormErrors = {
+        buyerName: '',
+        mobile: '',
+      }
+      for (let issue of result.error.issues) {
+        newFormErrors[issue.path[0]] = issue.message
+      }
+      setFormDataErrors(newFormErrors)
+
+      swal.fire({
+        icon: 'error',
+        title: '表單驗證失敗',
+        text: '請檢查並修正錯誤的欄位',
+      })
+      return // 阻止表單提交
+    }
     try {
-      const storedCart = JSON.parse(
+      const storedCart1 = JSON.parse(
+        // 從localStorage獲取購物車裡的數據
         localStorage.getItem('funeralshoppingcart') || '[]',
       )
-
+      // 將formData和storesCart組合成dataToSent
       const dataToSend = {
         ...formData,
-        countyId: formData.countyId,
-        cityId: formData.cityId,
-        cartItems: storedCart,
+        cartProjects: storedCart1,
       }
 
-      const response = await axios.post(
-        'http://localhost:3001/product/cartCheckout',
-        dataToSend,
+      // 首先發送到 /ecpay 路由
+      const ecpayResponse = await axios.get(
+        `http://localhost:3001/ecpay?${new URLSearchParams({ ...dataToSend, amount: totalPrice })}`, // 移除 ?amount
+        // 在請求體中傳遞 amount
       )
-
-      if (response.data.success) {
-        localStorage.setItem('funeralshoppingcart', '')
-        swal
-          .fire({
-            icon: 'success',
-            html: `
-            訂單已成功建立
-          `,
-            showCancelButton: true,
-            focusConfirm: false,
-            confirmButtonText: `
-            回首頁
-          `,
-            cancelButtonText: `
-            回商品列表
-          `,
-          })
-          .then((result) => {
-            if (result.isConfirm) {
-              router.push('/')
-            } else {
-              router.push('/funeral/')
-            }
-          })
+      console.log(ecpayResponse)
+      if (ecpayResponse.data.htmlContent) {
+        const tempDiv = document.createElement('div')
+        tempDiv.innerHTML = ecpayResponse.data.htmlContent
+        const form = tempDiv.querySelector('form')
+        if (form) {
+          document.body.appendChild(form)
+          form.submit()
+          // 清空localStorage
+          localStorage.removeItem('funeralshoppingcart')
+          // 清空Context中的購物車
+          clearCart1()
+        } else {
+          console.error('找不到支付表單')
+        }
       } else {
-        console.error('Checkout failed:', response.data.error)
-        // 處理錯誤
+        console.error('無效的回應格式')
       }
+
+      // 顯示成功消息並導航
+      // swal
+      //   .fire({
+      //     icon: 'success',
+      //     html: `訂單已成功建立`,
+      //     showCancelButton: true,
+      //     focusConfirm: false,
+      //     confirmButtonText: `回首頁`,
+      //     cancelButtonText: `回商品列表`,
+      //   })
+      //   .then((result) => {
+      //     if (result.isConfirm) {
+      //       router.push('/')
+      //     } else {
+      //       router.push('/estore/')
+      //     }
+      //   })
     } catch (error) {
-      console.error('Error during checkout:', error)
+      console.error('發生錯誤:', error)
       // 處理錯誤
     }
   }
@@ -179,108 +235,12 @@ export default function BookingList() {
   return (
     <Layout>
       {/* <ProgressBar /> */}
-
       <div
         className={`container-fluid d-flex flex-column flex-md-row justify-content-between align-items-stretch mb-5 ${styles.full}`}
       >
         <div className="row">
           {/* leftCard */}
           <div className="col-md-7 justify-content-center align-items-center">
-            {/* <!-- 付款方式 --> */}
-            <div
-              className="card my-3"
-              style={{
-                maxWidth: '100%',
-                height: 'auto',
-                borderTopRightRadius: 30 + 'px',
-                borderTopLeftRadius: 30 + 'px',
-              }}
-            >
-              <div
-                className="card-header text-center"
-                style={{
-                  backgroundColor: '#F6D554',
-                  color: '#ffffff',
-                  borderTopRightRadius: 30 + 'px',
-                  borderTopLeftRadius: 30 + 'px',
-                }}
-              >
-                付款方式
-              </div>
-              <div className="card-body" style={{ border: 10 + 'px' }}>
-                <h5 className="card-title mb-4">付款方式</h5>
-
-                <div className="form-check" style={{ marginBottom: '1rem' }}>
-                  <input
-                    className="form-check-input"
-                    type="radio"
-                    name="paymentMethod"
-                    id="creditCard"
-                    value="creditCard"
-                    style={{ marginTop: '0.3rem' }}
-                    checked={selectedPaymentMethod === 'creditCard'}
-                    onChange={() => setSelectedPaymentMethod('creditCard')}
-                  />
-                  <label className="form-check-label mb-2" htmlFor="creditCard">
-                    信用卡一次付清
-                  </label>
-                  {selectedPaymentMethod === 'creditCard' && (
-                    <>
-                      <input
-                        type="text"
-                        className="form-control rounded-pill"
-                        id="creditCardNumber"
-                        placeholder="請輸入信用卡號"
-                      />
-                      <form className="row">
-                        <div className="col-md-6">
-                          <label htmlFor="expiryDate" className="form-label">
-                            有效日期
-                          </label>
-                          <input
-                            type="text"
-                            className="form-control rounded-pill"
-                            id="expiryDate"
-                            placeholder="MM/YY"
-                          />
-                        </div>
-                        <div className="col-md-6">
-                          <label htmlFor="cvv" className="form-label">
-                            檢核碼
-                          </label>
-                          <input
-                            type="text"
-                            className="form-control rounded-pill"
-                            id="cvv"
-                            placeholder="CVV"
-                          />
-                        </div>
-                      </form>
-                    </>
-                  )}
-                </div>
-
-                <div className="form-check" style={{ marginBottom: '1rem' }}>
-                  <input
-                    className="form-check-input"
-                    type="radio"
-                    name="paymentMethod"
-                    id="otherMethods"
-                    value="otherMethods"
-                    style={{ marginTop: '0.3rem' }}
-                    checked={selectedPaymentMethod === 'otherMethods'}
-                    onChange={() => setSelectedPaymentMethod('otherMethods')}
-                  />
-                  <label
-                    className="form-check-label mb-2"
-                    htmlFor="otherMethods"
-                  >
-                    Line Pay
-                  </label>
-                </div>
-              </div>
-            </div>
-            {/* <!-- 付款方式 --> */}
             {/* <!-- 購買人資訊 --> */}
             <div
               className="card my-3"
@@ -316,7 +276,14 @@ export default function BookingList() {
                       className="form-control rounded-pill"
                       id="buyerName"
                       placeholder="姓名"
+                      value={formData.buyerName}
+                      onChange={handleInputChange}
                     />
+                    {formDataErrors.buyerName && (
+                      <div className="invalid-feedback">
+                        {formDataErrors.buyerName}
+                      </div>
+                    )}
                   </div>
                   {/* <!-- 手機 --> */}
                   <div className="col-md-6">
@@ -328,7 +295,14 @@ export default function BookingList() {
                       className="form-control rounded-pill"
                       id="mobile"
                       placeholder="手機"
+                      value={formData.mobile}
+                      onChange={handleInputChange}
                     />
+                    {formDataErrors.mobile && (
+                      <div className="invalid-feedback">
+                        {formDataErrors.mobile}
+                      </div>
+                    )}
                   </div>
                   {/* <!-- 市話 --> */}
                   <div className="col-md-6">
@@ -340,6 +314,7 @@ export default function BookingList() {
                       className="form-control rounded-pill"
                       id="telephone"
                       placeholder="市話"
+                      onChange={handleInputChange}
                     />
                   </div>
                   {/* <!-- 縣市 --> */}
@@ -391,6 +366,7 @@ export default function BookingList() {
                       className="form-control rounded-pill"
                       id="address"
                       placeholder="詳細地址"
+                      onChange={handleInputChange}
                     />
                   </div>
                   <div className="col-12">
@@ -608,9 +584,11 @@ export default function BookingList() {
                   fontWeight: 900,
                 }}
               >
-                {cartItems.length > 0 ? (
+                {/* 若cartProjects長度大於0, 則顯示購物車項目列表, 否則顯示"購物車是空的" */}
+                {cartProjects.length > 0 ? (
                   <>
-                    {cartItems.map((r, i) => (
+                    {/* 使用map遍歷cartProjects中的每個project項目 */}
+                    {cartProjects.map((r, i) => (
                       <div
                         key={i}
                         className="row align-items-center justify-content-center mb-3"
@@ -619,8 +597,9 @@ export default function BookingList() {
                           className="col-3 w-auto"
                           style={{ paddingRight: 2 + 'rem' }}
                         >
+                          {/* 項目圖片 */}
                           <img
-                            src={`http://localhost:3001/project/${card.project_id}.png`}
+                            src={`http://localhost:3001//project/${r.project_id}.png`}
                             alt="..."
                             className={styles.productImage}
                           />
@@ -628,14 +607,16 @@ export default function BookingList() {
                         <div className="col-9">
                           <div className="row">
                             <div className="col-12">
+                              {/* 項目名稱 */}
                               <div className={`fs-5 ${styles.productName}`}>
-                                {card.product_name}
+                                {r.project_name}
                               </div>
                             </div>
                             <div
                               className={`col-12 ${styles.quantityPriceContainer} mt-2`}
                             >
-                              <div
+                              {/* 項目數量 */}
+                              {/* <div
                                 className="justify-content-start fs-5"
                                 style={{
                                   color: '#FFF5CF',
@@ -647,12 +628,14 @@ export default function BookingList() {
                                   paddingBottom: 0.1 + 'rem',
                                 }}
                               >
-                                數量：{card.qty}
-                              </div>
+                                數量：{r.qty}
+                              </div> */}
+
+                              {/* 項目價格*項目數量 */}
                               <div
                                 className={`justify-content-end fs-4 ${styles.productPrice}`}
                               >
-                                $ {card.product_price * card.qty}
+                                $ {r.project_price * r.qty}
                               </div>
                             </div>
                           </div>
@@ -665,16 +648,6 @@ export default function BookingList() {
                   <p>購物車是空的</p>
                 )}
                 <hr />
-                <div className="col d-flex align-items-center">
-                  <div className="text-start m-2">
-                    <p className="card-text mb-1 fs-5">付款方式</p>
-                  </div>
-                  <div className="text-end ms-5 ms-auto">
-                    <p className="card-text fs-5">
-                      {paymentMethodMapping[selectedPaymentMethod] || ''}
-                    </p>
-                  </div>
-                </div>
                 <div className="col d-flex align-items-center">
                   <div className="text-start m-2">
                     <p className="card-text mb-1 fs-5">發票開立方式</p>
