9fcf4b807898fa2a4d62bf9c035b867763858abc
trent3281 <trent3281@gmail.com>
2024-07-20 11:13:50 +0800

    建立文章 表單驗證

--- pages/platform/create-article.js
+++ pages/platform/create-article.js
@@ -16,23 +16,57 @@ export default function CreateArticle() {
     article_img: null,
   })
 
+  const [myFormErrors, setMyFormErrors] = useState({
+    article_name: '',
+    article_content: '',
+    fk_class_id: '',
+  })
+
   const [imageFile, setImageFile] = useState(null) // 新增的狀態
   const [isSubmitted, setIsSubmitted] = useState(false)
 
   const onChange = (e) => {
-    if (e.target.name === 'article_img') {
+    const { name, value, files } = e.target
+    if (name === 'article_img') {
       // 判斷是否為圖片檔案的輸入
-      setImageFile(e.target.files[0])
+      setImageFile(files[0])
     } else {
       setMyForm({
         ...myForm,
-        [e.target.name]: e.target.value,
+        [name]: value,
       })
+
+      // 如果用户填写了某个字段，则清除相应的错误消息
+      if (value.trim() !== '') {
+        setMyFormErrors({
+          ...myFormErrors,
+          [name]: '',
+        })
+      }
     }
+  }
+
+  const validateForm = () => {
+    const errors = {}
+    if (!myForm.fk_class_id) {
+      errors.fk_class_id = '請選擇主題'
+    }
+    if (!myForm.article_name) {
+      errors.article_name = '標題不能空白'
+    }
+    if (!myForm.article_content) {
+      errors.article_content = '內容不能空白'
+    }
+    setMyFormErrors(errors)
+    return Object.keys(errors).length === 0
   }
 
   const onSubmit = async (e) => {
     e.preventDefault()
+
+    if (!validateForm()) {
+      return
+    }
 
     const formData = new FormData()
     formData.append('article_name', myForm.article_name)
@@ -90,32 +124,22 @@ export default function CreateArticle() {
                   id="inputGroupSelect01"
                   onChange={onChange}
                   name="fk_class_id"
+                  value={myForm.fk_class_id} // 確保選擇器的值與狀態一致
                 >
-                  <option value="" disabled selected>
+                  <option value="" disabled>
                     --選擇主題--
                   </option>
-                  <option value="1" defaultValue={myForm.fk_class_id === '1'}>
-                    寵物遺失
-                  </option>
-                  <option value="2" defaultValue={myForm.fk_class_id === '2'}>
-                    飼養心得
-                  </option>
-                  <option value="3" defaultValue={myForm.fk_class_id === '3'}>
-                    聊天討論
-                  </option>
-                  <option value="4" defaultValue={myForm.fk_class_id === '4'}>
-                    寵物健康醫療
-                  </option>
-                  <option value="5" defaultValue={myForm.fk_class_id === '5'}>
-                    寵物營養
-                  </option>
-                  <option value="6" defaultValue={myForm.fk_class_id === '6'}>
-                    寵物訓練
-                  </option>
-                  <option value="7" defaultValue={myForm.fk_class_id === '7'}>
-                    寵物相關新聞
-                  </option>
+                  <option value="1">寵物遺失</option>
+                  <option value="2">飼養心得</option>
+                  <option value="3">聊天討論</option>
+                  <option value="4">寵物健康醫療</option>
+                  <option value="5">寵物營養</option>
+                  <option value="6">寵物訓練</option>
+                  <option value="7">寵物相關新聞</option>
                 </select>
+                <div className="form-text text-danger ms-2">
+                  {myFormErrors.fk_class_id}
+                </div>
                 <div className="d-flex justify-content-center w-100">
                   <div className="w-100">
                     <label
@@ -124,7 +148,7 @@ export default function CreateArticle() {
                     >
                       文章標題
                     </label>
-                    <div className="input-group mb-3">
+                    <div className="input-group">
                       <input
                         type="text"
                         className="form-control rounded-pill"
@@ -134,13 +158,16 @@ export default function CreateArticle() {
                         onChange={onChange}
                       />
                     </div>
+                    <div className="form-text text-danger mb-3 ms-2">
+                      {myFormErrors.article_name}
+                    </div>
                     <label
                       htmlFor="article-content"
                       className="form-label ms-1 mt-1 mb-0"
                     >
                       文章內容
                     </label>
-                    <div className="input-group mb-3">
+                    <div className="input-group">
                       <textarea
                         style={{ height: 250 }}
                         id="article-content"
@@ -149,6 +176,9 @@ export default function CreateArticle() {
                         onChange={onChange}
                         className={`form-control ${styles.Rounded5}`}
                       />
+                    </div>
+                    <div className="form-text text-danger mb-3 ms-2">
+                      {myFormErrors.article_content}
                     </div>
                   </div>
                 </div>
