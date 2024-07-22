c392647ff9207ea68d4bc59248ba1ea687be8783
trent3281 <trent3281@gmail.com>
2024-07-22 23:38:32 +0800

    文章列捲動加載功能完成

--- components/platform/article-block.js
+++ components/platform/article-block.js
@@ -1,65 +1,126 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, useRef, useCallback } from 'react'
 import styles from '../../styles/platform/platform-style.module.css'
 import { BsBookmarkFill, BsChatText } from 'react-icons/bs'
 import moment from 'moment-timezone'
 import { ARTICLE } from '@/configs/platform/api-path'
 
 export default function ArticleBlock({ keyword }) {
-  const [data, setData] = useState({
-    success: false,
-    rows: [],
-  })
+  const [data, setData] = useState([])
+  const [page, setPage] = useState(1)
+  const [hasMore, setHasMore] = useState(true)
+  const observer = useRef()
+
+  const lastArticleElementRef = useCallback(
+    (node) => {
+      if (observer.current) observer.current.disconnect()
+      observer.current = new IntersectionObserver((entries) => {
+        if (entries[0].isIntersecting && hasMore) {
+          setPage((prevPage) => prevPage + 1)
+        }
+      })
+      if (node) observer.current.observe(node)
+    },
+    [hasMore],
+  )
 
   useEffect(() => {
-    fetch(`${ARTICLE}?keyword=${encodeURIComponent(keyword)}`)
+    fetch(`${ARTICLE}?keyword=${encodeURIComponent(keyword)}&page=${page}`)
       .then((r) => r.json())
       .then((myData) => {
-        setData(myData)
+        if (myData.rows.length === 0) {
+          setHasMore(false)
+        } else {
+          setData((prevData) => {
+            const newData = [...prevData, ...myData.rows]
+            return [...new Set(newData.map((item) => item.article_id))].map(
+              (id) => newData.find((item) => item.article_id === id),
+            )
+          })
+        }
       })
-  }, [keyword])
+      .catch((error) => console.error('Error loading articles:', error))
+  }, [keyword, page])
 
   return (
     <>
-      {data.rows.map((r) => {
+      {data.map((r, index) => {
         const dateFormat = moment(r.article_date).format('YYYY-MM-DD')
-        return (
-          <a
-            key={r.article_id}
-            className={`${styles.AReset}`}
-            href={`../../platform/article/${r.article_id}`}
-          >
-            <div className="m-2 border-bottom">
-              <div className="mx-2 d-flex">
-                <p className="me-3 border border-dark rounded-3">
-                  {r.class_name}
-                </p>
-                <p className={`${styles.LightGray}`}>{dateFormat}</p>
+        if (data.length === index + 1) {
+          return (
+            <a
+              ref={lastArticleElementRef}
+              key={r.article_id}
+              className={`${styles.AReset}`}
+              href={`../../platform/article/${r.article_id}`}
+            >
+              <div className="m-2 border-bottom">
+                <div className="mx-2 d-flex">
+                  <p className="me-3 border border-dark rounded-3">
+                    {r.class_name}
+                  </p>
+                  <p className={`${styles.LightGray}`}>{dateFormat}</p>
+                </div>
+                <div className="mx-3">
+                  <h2 className={`${styles.TitleOverHide} w-100 mb-3`}>
+                    {r.article_name}
+                  </h2>
+                  <p className={`${styles.ContentOverHide} mx-2`}>
+                    {r.article_content}
+                  </p>
+                </div>
+                <div className="d-flex mx-5">
+                  <p className={`me-5 ${styles.OrangeYellow}`}>
+                    <BsChatText className={`mb-1`} />
+                    {r.views_count}
+                  </p>
+                  <button
+                    className={`${styles.AReset} ${styles.LightGray} ${styles.FavHover} ${styles.BtnReset} mb-3`}
+                  >
+                    <BsBookmarkFill className={`mb-1`} />
+                    收藏
+                  </button>
+                </div>
               </div>
-              <div className="mx-3">
-                <h2 className={`${styles.TitleOverHide} w-100 mb-3`}>
-                  {r.article_name}
-                </h2>
-                <p className={`${styles.ContentOverHide} mx-2`}>
-                  {r.article_content}
-                </p>
+            </a>
+          )
+        } else {
+          return (
+            <a
+              key={r.article_id}
+              className={`${styles.AReset}`}
+              href={`../../platform/article/${r.article_id}`}
+            >
+              <div className="m-2 border-bottom">
+                <div className="mx-2 d-flex">
+                  <p className="me-3 border border-dark rounded-3">
+                    {r.class_name}
+                  </p>
+                  <p className={`${styles.LightGray}`}>{dateFormat}</p>
+                </div>
+                <div className="mx-3">
+                  <h2 className={`${styles.TitleOverHide} w-100 mb-3`}>
+                    {r.article_name}
+                  </h2>
+                  <p className={`${styles.ContentOverHide} mx-2`}>
+                    {r.article_content}
+                  </p>
+                </div>
+                <div className="d-flex mx-5">
+                  <p className={`me-5 ${styles.OrangeYellow}`}>
+                    <BsChatText className={`mb-1`} />
+                    {r.views_count}
+                  </p>
+                  <button
+                    className={`${styles.AReset} ${styles.LightGray} ${styles.FavHover} ${styles.BtnReset} mb-3`}
+                  >
+                    <BsBookmarkFill className={`mb-1`} />
+                    收藏
+                  </button>
+                </div>
               </div>
-
-              <div className="d-flex mx-5">
-                <p className={`me-5 ${styles.OrangeYellow}`}>
-                  <BsChatText className={`mb-1`} />
-                  {r.views_count}
-                </p>
-
-                <button
-                  className={`${styles.AReset} ${styles.LightGray} ${styles.FavHover} ${styles.BtnReset} mb-3`}
-                >
-                  <BsBookmarkFill className={`mb-1`} />
-                  收藏
-                </button>
-              </div>
-            </div>
-          </a>
-        )
+            </a>
+          )
+        }
       })}
     </>
   )
