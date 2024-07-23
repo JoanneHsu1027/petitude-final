f898b5eb1592d9645a39054987d9902451a30e20
trent3281 <trent3281@gmail.com>
2024-07-23 19:04:29 +0800

    u

--- pages/index.js
+++ pages/index.js
@@ -12,26 +12,28 @@ export default function home() {
           z-index: 999999;
         }
       `}</style>
-      <Layout>
-        <Funeral />
+      <div style={{ backgroundImage: 'url(/index/bg-img.png)' }}>
+        <Layout>
+          <Funeral />
 
-        {/* 論壇區塊 start */}
-        <div className="container my-5">
-          <div className="row d-flex justify-content-center">
-            <div className="col-xl-10 col-lg-12 position-relative">
-              <a href="./platform">
-                <img
-                  className="TopCenter start-50 translate-middle"
-                  src="../forum-pic/forum-title.png"
-                  alt=""
-                />
-              </a>
-              <ClassSection />
+          {/* 論壇區塊 start */}
+          <div className="container my-5">
+            <div className="row d-flex justify-content-center">
+              <div className="col-xl-9 col-lg-10 col-md-12 position-relative">
+                <a href="./platform">
+                  <img
+                    className="TopCenter start-50 translate-middle"
+                    src="../index/forum-title.png"
+                    alt=""
+                  />
+                </a>
+                <ClassSection />
+              </div>
             </div>
           </div>
-        </div>
-        {/* 論壇區塊 end */}
-      </Layout>
+          {/* 論壇區塊 end */}
+        </Layout>
+      </div>
     </>
   )
 }
