6e5f085272cf8e5c3a7b9cb23bad4ed88b9db659
Angela <angelakao519@gmail.com>
2024-07-26 16:25:35 +0800

    修改手機板輪播

--- components/funeral/funeral/carousel.js
+++ components/funeral/funeral/carousel.js
@@ -5,27 +5,27 @@ export default function Carousel() {
   return (
     <>
       {/* <!-- carousel --> */}
-      <div className="containerp-0">
+      <div className="container-fluid p-0">
         <div className="row m-0">
           <div className="col-12 p-0">
             <div
               id="carouselExampleSlidesOnly"
               className="carousel slide"
               data-bs-ride="carousel"
-              data-bs-interval="1000" // 設置輪播速度為 1000 毫秒（1 秒）
+              data-bs-interval="500" // 设置轮播速度为 1000 毫秒（1 秒）
             >
               <div className="carousel-inner">
                 <div className="carousel-item active">
                   <img
                     src="/funeral/appointment1.png"
-                    className="d-block w-100"
+                    className="d-block w-100 img-fluid"
                     alt="..."
                   />
                 </div>
                 <div className="carousel-item">
                   <img
                     src="/funeral/appointment2.png"
-                    className="d-block w-100"
+                    className="d-block w-100 img-fluid"
                     alt="..."
                   />
                 </div>
