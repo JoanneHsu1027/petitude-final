105bbf24cb78617a1a2a9793f283a2f791430fe7
trent3281 <trent3281@gmail.com>
2024-07-26 20:00:26 +0800

    re message block

--- pages/platform/re_message/[message_id].js
+++ pages/platform/re_message/[message_id].js
@@ -34,13 +34,16 @@ export default function MessageId({ message_id }) {
   return (
     <>
       {/* 回覆留言區塊 */}
-      {replies.map((reply, index) => (
-        <div key={index} className="d-flex border-bottom mt-2 mx-1 px-2">
+      {replies.map((reply) => (
+        <div
+          key={reply.re_message_id}
+          className="d-flex border-bottom mt-2 mx-1 px-2"
+        >
           <div className="me-2">
             <img src="/forum-pic/avatar.png" alt="" />
           </div>
           <div className="flex-grow-1 me-2">
-            <p>b2c_name</p>
+            <p>{reply.b2c_name}</p>
             <p>{reply.re_message_content}</p>
             <div className="d-flex">
               <p className="me-4">
